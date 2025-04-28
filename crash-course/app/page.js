dcf9207287c1f194a127c6046ae3328cd16f4290
momin0933 <apon.cse09@gmail.com>
2025-04-28 17:48:37 +0600

    updated
    updated

---
+++ crash-course/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <div className="p-6">Hello WorldHello World Hello World Hello World Hello World </div>
+    </main>
+  );
+}
